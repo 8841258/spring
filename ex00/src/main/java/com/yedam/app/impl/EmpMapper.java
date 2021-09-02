fad460508a8d79cdb4defcf1c51dd28d6408fcfa
jimin <8841258@naver.com>
2021-09-02 11:32:51 +0900

    20210902

--- ex00/src/main/java/com/yedam/app/impl/EmpMapper.java
+++ ex00/src/main/java/com/yedam/app/impl/EmpMapper.java
@@ -3,13 +3,17 @@ package com.yedam.app.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import com.yedam.app.Departments;
 
 @Repository //DAO
 public interface EmpMapper {
-	List<Map> getEmp();
+	List<Map> getEmp(String deptid);
 	
 	List<Departments> getDept();
+	
+	@Select("select sysdate from dual")
+	public String getTime();
 }
--- ex00/src/test/java/com/yedam/app/EmpMapperClient.java
+++ ex00/src/test/java/com/yedam/app/EmpMapperClient.java
@@ -1,7 +1,12 @@
 package com.yedam.app;
 
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,10 +21,25 @@ import lombok.extern.java.Log;
 public class EmpMapperClient {
 
 	@Autowired EmpMapper dao;
+	@Autowired SqlSessionTemplate mybatis;
 	
 	@Test
+	public void mybatisTest() {
+//		List<Employees> list = mybatis.selectList("com.yedam.app.impl.EmpMapper.getEmp", "10");
+		List<Departments> list2 = mybatis.selectList("com.yedam.app.impl.EmpMapper.getDept");
+		log.info(list2.get(0).toString());
+//		log.info("job_id = " + list.get(0).getJobId());
+		
+	}
+	
+	@Test @Ignore
 	public void test() {
-		log.info(dao.getDept().toString());
+		log.info(dao.getEmp("50").toString());
+	}
+	
+	@Test @Ignore
+	public void getTime() {
+		log.info(dao.getTime());
 	}
 
 }
